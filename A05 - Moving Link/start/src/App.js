aeb752bcc7a11ae37a18d7ce9847a69130a88d04
Jake <jacob.b.singer@gmail.com>
2020-09-29 13:56:42 -0400

    small change
    small change

--- A05 - Moving Link/start/src/App.js
+++ A05 - Moving Link/start/src/App.js
@@ -1,16 +1,54 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
 
 export default function App() {
+  const canvasRef = useRef(null);
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+
+  useEffect(() => {
+    const context = canvasRef.current.getContext('2d');
+    context.canvas.height = window.innerHeight;
+    context.canvas.width = window.innerWidth;
+  }, []);
+
+  useEffect(() => {
+    const context = canvasRef.current.getContext('2d');
+    context.clearRect(0, 0, window.innerWidth, window.innerHeight);
+
+    context.fillRect(x, y, 100, 100);
+  }, [x, y]);
+
+  //add event listener on a window to listen for arrow key
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+
+    function handleKeyDown(e) {
+      if (e.key === 'ArrowUp') move('Up');
+      if (e.key === 'ArrowLeft') move('Left');
+      if (e.key === 'ArrowDown') move('Down');
+      if (e.key === 'ArrowRight') move('Right');
+    }
+
+    return () => window.removeEventListener('keydown', handleKeyDown);
+  }, []);
+
+  function move(direction) {
+    if (direction === 'Up') setY((y) => y - 20);
+    if (direction === 'Left') setX((x) => x - 20);
+    if (direction === 'Down') setY((y) => y + 20);
+    if (direction === 'Right') setX((x) => x + 20);
+  }
+
   return (
     <div className="app">
-      <canvas />
+      <canvas ref={canvasRef} />
 
       <div className="arrows">
-        <button>Up</button>
-        <button>Left</button>
-        <button>Down</button>
-        <button>Right</button>
+        <button onClick={() => move('Up')}>Up</button>
+        <button onClick={() => move('Left')}>Left</button>
+        <button onClick={() => move('Down')}>Down</button>
+        <button onClick={() => move('Right')}>Right</button>
       </div>
 
       <div className="images">
